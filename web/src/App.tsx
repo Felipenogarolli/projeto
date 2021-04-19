a57dbc9350f3ed1f4bbed2291071a430c06cb833
heloisebonato <43188658+heloisebonato@users.noreply.github.com>
2021-04-19 20:27:20 -0300

    Update tela inicio

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
-import {Container, Row, Col, Image, Button } from "react-bootstrap";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 import logoinicio from './img/logo-inicio.jpeg';
-
+import nuvem1 from './img/nuvem1.jpeg';
+import imagemInicio from './img/mapa-inicio.jpeg';
 
 
 function App() {
@@ -12,12 +13,24 @@ function App() {
       <div className="sec01">
         <Container>
           <Row>
-            <Col lg={6} className="">
+            <Col lg={5} className="mt-4">
               <img className="logoinicio" src={logoinicio} alt="logo" />
 
-              <main className="text01">
+              <main className="text01 mt-5">
                 <p>Adicionar um resump sobre o que é a safe city</p>
               </main>
+              <Row>
+                <Col lg={6}>
+                  <a className="btn-login" href="#">Entrar</a>
+                </Col>
+                <Col lg={6}>
+                  <a className="btn-register" href="#">Cadastre-se</a>
+                </Col>
+              </Row>
+              <img className="img-canto" src={nuvem1} />
+            </Col>
+            <Col lg={7}>
+              <img className="imagem-inicio" src={imagemInicio} />
             </Col>
           </Row>
         </Container>
