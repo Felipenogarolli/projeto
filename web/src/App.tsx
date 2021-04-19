567e083fb4b9085ec60679ca0d98ef26c265cc90
Guipb10 <gui.pb1@hotmail.com>
2021-04-19 19:54:01 -0300

    teste.1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
 import logoinicio from './img/logo-inicio.jpeg';
-import nuveminicio from './img/nuvem2.jpeg';
+
 
 
 function App() {
