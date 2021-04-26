3e6ac8006a1fff57d76e3f89203cab347e326cd1
heloisebonato <43188658+heloisebonato@users.noreply.github.com>
2021-04-26 20:35:46 -0300

    mobile tela inicial

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -15,37 +15,38 @@ function App() {
     <div id="page-landing">
       <div className="sec01">
         <Container>
-          <Row className="mb-5 pb-5">
-            <Col lg={5} className="mt-4">
+          <Row className="mb-lg-5 pb-5">
+            <Col lg={5} className="mt-5">
               <img className="logoinicio" src={logoinicio} alt="logo" />
 
-              <main className="text01 mt-5">
-                <p>Adicionar um resump sobre o que é a safe city</p>
-              </main>
-              <Row>
-                <Col lg={6}>
+              <p className="text01 mt-3 mb-lg-5">
+                Ajude você também a tornar a sua cidade um local mais seguro
+               </p>
+               <img alt="img-inicio" className="imagem-inicio img-fluid d-block d-lg-none" src={imagemInicio} />
+              <Row className="my-3">
+                <Col xs={5} className="p-lg-0">
                   <Button className="btn-login" href="#">Entrar</Button>
                 </Col>
-                <Col lg={6}>
+                <Col xs={5}>
                   <Button className="btn-register" href="#">Cadastre-se</Button>
                 </Col>
               </Row>
             </Col>
             <Col lg={7}>
-              <img alt="img-inicio" className="imagem-inicio img-fluid" src={imagemInicio} />
+              <img alt="img-inicio" className="imagem-inicio img-fluid d-none d-lg-block" src={imagemInicio} />
             </Col>
           </Row>
           <Row>
             <Col lg={5}></Col>
-            <Col lg={7} className="mt-lg-5">
-              <div className="redes d-flex justify-content-end align-items-center px-5">
-                <img className="icon mr-4" src={iconLinkedin} />
-                <img className="icon mr-4" src={iconFacebook} />
-                <img className="icon mr-4" src={iconInstagram} />
+            <Col lg={7}>
+              <div className="redes pt-5 pt-lg-0 d-flex justify-content-end align-items-center px-lg-5">
+                <img className="icon mr-3 mr-lg-4" src={iconLinkedin} />
+                <img className="icon mr-3 mr-lg-4" src={iconFacebook} />
+                <img className="icon mr-3 mr-lg-4" src={iconInstagram} />
               </div>
             </Col>
           </Row>
-          <img alt="img-nuvem2" className="img-nuvem-right img-fluid" src={nuvem2} />
+          <img alt="img-nuvem2" className="img-nuvem-right img-fluid d-none d-lg-block" src={nuvem2} />
           <img alt="img-canto" className="img-canto img-fluid" src={nuvem1} />
         </Container>
       </div>
