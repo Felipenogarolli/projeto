b1e32f9f64ff1c0129bd110c6e6fa522768d6296
Vinícius Joay <43188303+viniciusjoay@users.noreply.github.com>
2021-06-07 19:35:03 -0300

    Hash introduzindo
    As senhas agora são armazenadas de forma segura.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,20 @@ const connection = require('./database/database');
 const Register = require('./database/Register');
 const cors = require('cors');
 const alert = require('alert');
+const bcrypt = require("bcryptjs")
+const session = require('express-session');
+
+//Session
+app.use(session({
+    secret: "expcriativabsi2021",
+    cookie: {maxAge: 24 * 60 * 60 * 1000},//24 horas
+    resave: true,
+    saveUninitialized: false
+}))
+
+app.get("/session", (req, res) =>{
+    
+});
 
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
@@ -19,21 +33,26 @@ connection
 
 app.post('/register', (req,res) =>{
     const {email,password,username,phone,confirmPassword} = req.body;
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
     Register.findOne({where:{email:email}}).then( user => {
         if(user == undefined && password == confirmPassword){
             Register.create({
-                email,
-                password,
+                email,  
+                password: hash,
                 username,
                 phone
             }).then((data) =>{
                 res.json(data);
+                alert("Usuário registrado!")
             });
         }else{
             alert("Este e-mail já foi cadastrado ou as senhas não coincidem");
         }
     })
 });
+
 //Servidor
 app.listen(3001, () => {
     console.log("Server running on port 3001");
